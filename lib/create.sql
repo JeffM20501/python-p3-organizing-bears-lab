98bf24f1964398ffbdc1fbe691e38b1abc4f2f13
JeffM20501 <kimmuna440@gmail.com>
2025-12-30 17:37:23 +0300

    completed lab

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears(
+    id INT PRIMARY KEY, 
+    name TEXT,
+    age int,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive INT
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr.Chocolate', 10, 'M', 'black', 'low', 1),
+('Rowdy', 5, 'M', 'black', 'mid', 1),
+('Tabitha', 10, 'F', 'white', 'high', 1),
+('Sergeant Brown', 3, 'M', 'black white', 'high', 1),
+('Melissa', 6, 'F', 'brown', 'MID', 1),
+('Grinch', 11, 'M', 'black', 'low', 0),
+('Wendy', 4, 'F', 'black', 'High', 1),
+(NULL, 10, 'M', 'black', 'low', 1);
